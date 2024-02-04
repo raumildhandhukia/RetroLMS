bc3d9b8ca4231ffc05f6e3ee104d74dd8063a57b
Dhairya Dudhatra <dhairyadudhatra@gmail.com>
2024-02-04 12:52:21 -0700

    Route files for better visibility into each routes. Taiga taskid => 9

---
+++ back/routes/adminRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Admin Routes
+router.post("/instructors", (req, res) => {
+  // Create Instructor
+});
+
+module.exports = router;
