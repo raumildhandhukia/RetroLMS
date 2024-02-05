4675ef0c075bd603a4d8d71393960a68b8d25a06
Raumil D <rdhandhu@asu.edu>
2024-02-05 13:56:23 -0700

    Update package.json

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -5,7 +5,11 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true, index: true },
     password: { type: String, required: true },
-    role: { type: Strinxg, enum: ["student", "instructor", "admin"], required: true },
+    role: {
+      type: Strinxg,
+      enum: ["student", "instructor", "admin"],
+      required: true,
+    },
     profile: {
       firstName: { type: String, required: true },
       lastName: { type: String, required: true },
