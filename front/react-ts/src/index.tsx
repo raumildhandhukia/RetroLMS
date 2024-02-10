d91887c9548b762f590179cb2f9484ea057bf52a
rohith7869 <rbellapu@asu.edu>
2024-02-09 23:29:03 -0700

    Task #42 Creation of Signal Login Page

--- front/react-ts/src/index.tsx
+++ front/react-ts/src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Login from './Login';
+import { BrowserRouter, Route, Routes } from"react-router-dom";
+import InstructorDashboard from "./InstructorDashboard";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <BrowserRouter>
+          <Routes>
+              <Route index element={<Login />} />
+              <Route path="/" element={<Login />} />
+              <Route path="/instructorDashboard" element={<InstructorDashboard />} />
+              <Route path="/courses" element={<InstructorDashboard />} />
+          </Routes>
+      </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
