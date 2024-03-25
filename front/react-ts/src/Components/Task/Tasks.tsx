a0b629c8147c9874fb082488de82c1c61c4b763d
rohith7869 <rbellapu@asu.edu>
2024-03-24 18:28:14 -0700

    #144 Fetching task data from db

--- front/react-ts/src/Components/Task/Tasks.tsx
+++ front/react-ts/src/Components/Task/Tasks.tsx
@@ -21,8 +21,66 @@ const Tasks: React.FC = () => {
   const [courseName, setCourseName] = useState<string>('SER517');
   const [role, setRole] = useState<string>('');
   const navigate = useNavigate();
+  useEffect(() => {
+    const checkProfile = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/profile", {
+          method: "GET",
+          credentials: "include", // Include cookies in the request
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setRole(data.role);
+        } else {
+          console.log("User not found");
+        }
+      } catch (error) {
+        console.error("Error checking for profile", error);
+      }
+    };
+
+    checkProfile();
+  }, []);
 
   useEffect(() => {
+    // Function to fetch tasks from the server
+    const fetchTasks = async () => {
+      try {
+
+        //fetch course by calling '/coursesById' and use this courseID to fetch all the Items
+        const courseId = await fetch('http://localhost:8080/coursesById', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        //const courseName = '65d7c8254df4ea811a701b00'; // Replace with the actual courseName
+        const response = await fetch(`http://localhost:8080//task/get/${courseId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch items');
+        }
+
+        setTasks(await response.json()); //await response.json();
+        //const updatedTasks = tasks.map(task => ({ ...task, course: courseName}));
+        //setTasks(updatedTasks); // Update tasks state with the received data
+      } catch (error) {
+        console.error('Error fetching items:', error);
+        // You can handle the error appropriately (e.g., show an error message)
+      }
+    };
+
+    // Call the fetchTasks function when the component mounts
+    fetchTasks();
+  }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
+
+
+  /*useEffect(() => {
     // Function to fetch tasks from the server
     const fetchTasks = async () => {
       try {
@@ -54,10 +112,11 @@ const Tasks: React.FC = () => {
   }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
 
   //setTasks(data.tasks);
+   */
   return (
       role === "student" ?
-          <TaskList tasks={data.tasks} courseName={courseName} /> :
-    <InstructorTaskList tasks={data.tasks} courseName={courseName} />
+          <TaskList tasks={tasks} courseName={courseName} /> :
+    <InstructorTaskList tasks={tasks} courseName={courseName} />
   );
 };
 
