713f18255b65cb334a74dcc906943951cb9e576a
Raumil D <rdhandhu@asu.edu>
2024-04-17 23:47:36 -0700

    Leaderboard UI Enhancement: Added Rank Icons

--- front/react-ts/src/Components/Leaderboard/Leaderboard.tsx
+++ front/react-ts/src/Components/Leaderboard/Leaderboard.tsx
@@ -11,6 +11,24 @@ interface StudentScore  {
     name: string;
     score: number;
   }
+
+const getPNGbyRank = (rank: number) => {
+  if (rank === 1) {
+    return require('./rankImagesPNG/rank1.png');
+  } else if (rank === 2) {
+    return require('./rankImagesPNG/rank2.png');
+  } else if (rank === 3) {
+    return require('./rankImagesPNG/rank3.png');
+    } else if (rank === 4) {
+    return require('./rankImagesPNG/rank4.png');
+    } else if (rank === 5) {
+    return require('./rankImagesPNG/rank5.png');
+  } else {
+    return require('./rankImagesPNG/other.png');
+  } 
+
+}
+
 const Leaderboard: React.FC<LBProps> = ({courseId}) => {
   const [data, setData] = useState<StudentScore[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
@@ -60,8 +78,12 @@ const renderLeaderboard = () => {
             width: '80%',
             textAlign: 'center'
           }}>
-            <p className="title" style={{color:'red'}}>{index + 1}</p>
-              <p style={{color:'yellow', margin:'-10px 0px 8px 0px'}}>{student.name}</p>
+            <p className="title" style={{color:'red', marginLeft:'5px'}}>{index + 1}</p>
+            <img src={getPNGbyRank(index + 1)} alt="Rank" style={{
+              width: '40px',
+              margin: '-15px 0px -5px 0px',
+            }}/>
+              <p style={{color:'yellow', margin:'-30px 0px 18px 0px'}}>{student.name}</p>
           </div>
           <div className="nes-container is-rounded with-title is-dark" style={{
             textAlign: 'center',
@@ -69,7 +91,7 @@ const renderLeaderboard = () => {
             <p className="title" style={{
               color: 'red',
             }}>Score</p>
-            <p style={{color:'yellow', margin:'-10px 0px 8px 0px'}}>{student.score}</p>
+            <p style={{color:'yellow', margin:'0px 0px 8px 0px'}}>{student.score}</p>
           </div>
         </div>
       ))}
